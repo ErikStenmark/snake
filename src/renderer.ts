b9f18a9b5964cad0326384ad8cf3a69ac51750b1
Erik Stenmark <erik.stenmark@gmai.com>
2022-04-24 17:31:56 +0300

    fixed misalignments of snake, food and playfield with a gridsystem

--- src/renderer.ts
+++ src/renderer.ts
@@ -2,7 +2,7 @@ import { Electron } from './preload';
 import Canvas from './canvas';
 
 declare global { interface Window { electron: Electron; } }
-
+type Position = { x: number; y: number };
 class Renderer {
   private canvas: Canvas;
   private ctx: CanvasRenderingContext2D;
@@ -24,13 +24,11 @@ class Renderer {
 
   private keysPressed: string[] = [];
 
-  private tickRate = 60;
+  private tickRate = 20;
   private elapsedDelta: number = 0;
 
-  private snakePos: Array<{ x: number, y: number }> = [];
+  private snakePos: Position[] = [];
 
-  private snakeYPos = 0;
-  private snakeXPos = 0;
   private snakeDir = 'r';
   private snakeSize = 4;
 
@@ -38,8 +36,9 @@ class Renderer {
   private playFieldWidth = 0;
   private blockSize = 0;
 
-  private firstRender = true;
+  private coordinates: Position[] = [];
 
+  private firstRender = true;
 
   private boundaries = {
     xStart: 0,
@@ -48,10 +47,13 @@ class Renderer {
     yEnd: 0
   }
 
-  private foodPos: { x: number | undefined, y: number | undefined } = {
-    x: undefined,
-    y: undefined
-  };
+
+  private noFoodPos: Position = {
+    x: -1,
+    y: -1
+  }
+
+  private foodPos: Position = this.noFoodPos;
 
   constructor() {
     this.canvas = new Canvas();
@@ -61,6 +63,10 @@ class Renderer {
     this.setSize();
     window.addEventListener('resize', () => {
       this.setSize();
+      this.calculatePlayArea();
+      this.getAllCoordinates();
+      this.foodPos = this.noFoodPos;
+      this.snakePos[this.snakePos.length - 1] = this.coordinates[Math.round(this.coordinates.length / 2)];
     });
 
     window.addEventListener('keydown', e => {
@@ -81,17 +87,22 @@ class Renderer {
     this.canvas.fill();
     this.displayFPS();
     this.drawPlayField();
+
+    if (this.firstRender) {
+      this.calculatePlayArea();
+      this.getAllCoordinates();
+    }
+
+    this.drawCoordinates();
     this.snake();
     this.food();
-    this.firstRender = false;
+
+    if (this.firstRender) {
+      this.firstRender = false;
+    }
   }
 
-  private drawPlayField() {
-    const lineWidth = 1;
-    this.ctx.lineWidth = lineWidth;
-    this.ctx.strokeStyle = 'white';
-    this.ctx.fillStyle = 'white';
-
+  private calculatePlayArea() {
     const smallerSide = this.width <= this.height ? this.width : this.height;
     const sideLength = Math.floor(smallerSide);
     const margin = Math.floor(smallerSide * 0.1);
@@ -103,6 +114,13 @@ class Renderer {
     this.boundaries.yStart = (this.height - sideLength) / 2 + margin;
     this.boundaries.xEnd = this.boundaries.xStart + this.playFieldWidth;
     this.boundaries.yEnd = this.boundaries.yStart + this.playFieldWidth;
+  }
+
+  private drawPlayField() {
+    const lineWidth = 1;
+    this.ctx.lineWidth = lineWidth;
+    this.ctx.strokeStyle = 'white';
+    this.ctx.fillStyle = 'white';
 
     this.ctx.strokeRect(
       this.boundaries.xStart - lineWidth,
@@ -111,6 +129,36 @@ class Renderer {
       this.playFieldWidth + lineWidth + 1
     );
   }
+
+  private drawCoordinates() {
+    this.ctx.strokeStyle = '#333';
+    this.ctx.lineWidth = 1;
+
+    this.coordinates.forEach(pos => {
+      this.ctx.strokeRect(pos.x + 1, pos.y + 1, this.getSnakeSize() - 2, this.getSnakeSize() - 2);
+    })
+  }
+
+  private getAllCoordinates() {
+    const coordinates: Position[] = [];
+
+    let y = this.boundaries.yStart;
+
+    while (y < this.boundaries.yEnd) {
+      let x = this.boundaries.xStart;
+
+      while (x < this.boundaries.xEnd) {
+        coordinates.push({ x, y });
+        x += this.getSnakeSize();
+      }
+
+      y += this.getSnakeSize();
+    }
+
+    console.log('coordinates length:', coordinates.length);
+    this.coordinates = coordinates;
+  }
+
 
   private setSize() {
     this.height = window.innerHeight;
@@ -146,14 +194,6 @@ class Renderer {
     this.ctx.font = `${fontSize}px arial`;
     this.ctx.fillText(`score: ${this.snakePos.length - 1}`, this.boundaries.xStart, 0 + double);
     this.ctx.textAlign = 'right';
-    this.ctx.fillText(`bound: ${JSON.stringify(this.boundaries)}`, this.width - double, 0 + double);
-    this.ctx.fillText(
-      `snake: 
-    xS: ${this.snakeXPos} 
-    xE: ${this.snakeXPos + this.snakeSize}
-    yS: ${this.snakeYPos} 
-    yE: ${this.snakeYPos + this.snakeSize}
-    `, this.width - double, this.height - double);
   }
 
   private roundDownToMultiple(number: number, multiple: number) {
@@ -161,16 +201,48 @@ class Renderer {
   }
 
   private food() {
-    if (!this.foodPos.x && !this.foodPos.y) {
-      const maxX = this.boundaries.xEnd - this.snakeSize;
-      const maxY = this.boundaries.yEnd - this.snakeSize;
-      this.foodPos.x = this.roundDownToMultiple(this.randomIntFromInterval(this.boundaries.xStart, maxX), this.snakeSize);
-      this.foodPos.y = this.roundDownToMultiple(this.randomIntFromInterval(this.boundaries.yStart, maxY), this.snakeSize) + 2;
+    let foodPos = this.foodPos;
+    let originalCoordinatePosition = 0;
+    let coordinatePosition = 0;
+
+    const needsNewFood = (
+      foodPos.x === this.noFoodPos.x &&
+      foodPos.y === this.noFoodPos.y &&
+      this.coordinates.length
+    );
+
+
+    if (needsNewFood) {
+      coordinatePosition = this.randomIntFromInterval(0, this.coordinates.length - 1);
+      originalCoordinatePosition = coordinatePosition;
+
+      foodPos = this.coordinates[coordinatePosition];
+
+      while (this.snakeIncludesCoordinates(foodPos)) {
+        coordinatePosition += 1
+        foodPos = this.coordinates[coordinatePosition];
+
+        if (coordinatePosition > this.coordinates.length - 1) {
+          coordinatePosition = 0;
+          foodPos = this.coordinates[coordinatePosition];
+        }
+
+        if (coordinatePosition === originalCoordinatePosition) {
+          foodPos = this.noFoodPos;
+        }
+      }
+
+      this.foodPos = foodPos;
+    }
+
+
+    if (this.snakeIncludesCoordinates(this.foodPos)) {
+      this.foodPos = this.coordinates.find(c => !this.snakeIncludesCoordinates(c)) || this.noFoodPos;
     }
 
     this.ctx.fillStyle = 'red';
-    if (!!this.foodPos.x && !!this.foodPos.y) {
-      this.ctx.fillRect(this.foodPos.x, this.foodPos.y, this.snakeSize, this.snakeSize);
+    if (this.foodPos.x >= 0 && this.foodPos.y >= 0) {
+      this.ctx.fillRect(this.foodPos.x, this.foodPos.y, this.getSnakeSize(), this.getSnakeSize());
     }
   }
 
@@ -191,21 +263,34 @@ class Renderer {
     const gotFood = headX === this.foodPos.x && headY === this.foodPos.y;
 
     if (gotFood) {
-      this.foodPos.x = undefined;
-      this.foodPos.y = undefined;
+      this.foodPos = this.noFoodPos;
     }
 
     return gotFood;
   }
 
+  private coordinatesIncludes(coordinates: Position[], position: Position) {
+    return !!coordinates.find(c => c.x === position.x && c.y === position.y)
+  }
+
+  private snakeIncludesCoordinates(pos: Position) {
+    return this.coordinatesIncludes(this.snakePos, pos);
+  }
+
+  private playFieldIncludesCoordinates(pos: Position) {
+    return this.coordinatesIncludes(this.coordinates, pos);
+  }
+
+  private getSnakeSize() {
+    return this.snakeSize * this.blockSize;
+  }
+
   private snake() {
-    this.snakeSize = 4 * this.blockSize;
+    const snakeSize = this.getSnakeSize();
 
     if (this.firstRender) {
-      this.snakePos.push({
-        x: this.boundaries.xStart + 4 * this.snakeSize,
-        y: this.snakeYPos = this.boundaries.yEnd - 4 * this.snakeSize
-      });
+      console.log(this.coordinates);
+      this.snakePos.push(this.coordinates[1]);
     }
 
     const { x: headX, y: headY } = this.getSneakHead();
@@ -214,8 +299,8 @@ class Renderer {
     this.snakePos.forEach((pos, i) => {
       this.ctx.fillStyle = i === this.snakePos.length - 1 ? 'lime' : 'darkgreen';
       this.ctx.strokeStyle = 'black';
-      this.ctx.fillRect(pos.x, pos.y, this.snakeSize, this.snakeSize);
-      this.ctx.strokeRect(pos.x + 1, pos.y + 1, this.snakeSize - 1, this.snakeSize - 1);
+      this.ctx.fillRect(pos.x, pos.y, snakeSize, snakeSize);
+      this.ctx.strokeRect(pos.x + 1, pos.y + 1, snakeSize - 1, snakeSize - 1);
     });
 
     const latestKey = this.keysPressed.pop();
@@ -223,6 +308,7 @@ class Renderer {
     if (latestKey === 'ArrowUp' && this.snakeDir !== 'd') {
       this.snakeDir = 'u'
     }
+
 
     if (latestKey === 'ArrowDown' && this.snakeDir !== 'u') {
       this.snakeDir = 'd'
@@ -241,40 +327,37 @@ class Renderer {
     const shouldMoveUp = this.snakeDir === 'u';
     const shouldMoveDown = this.snakeDir === 'd';
 
-    const isWithinXEnd = headX + this.snakeSize < this.boundaries.xEnd;
-    const isWithinXStart = headX > this.boundaries.xStart;
-    const isWithinYStart = headY > this.boundaries.yStart;
-    const isWithinYEnd = headY + this.snakeSize < this.boundaries.yEnd;
+    const nextPos = { x: headX, y: headY };
+    const movementAmount = snakeSize;
 
-    const movementAmount = this.snakeSize;
+    const moveTo = (pos: Position) => {
+      if (!this.snakeIncludesCoordinates(pos) && this.playFieldIncludesCoordinates(pos)) {
+        this.snakePos.push(pos);
+        if (!this.eat()) {
+          this.snakePos.shift();
+        }
+      }
+    }
 
     if (this.elapsedDelta >= this.tickRate) {
-      if (shouldMoveRight && isWithinXEnd) {
-        this.snakePos.push({ x: headX + movementAmount, y: headY });
-        if (!this.eat()) {
-          this.snakePos.shift();
-        }
+      if (shouldMoveRight) {
+        nextPos.x = headX + movementAmount;
+        moveTo(nextPos);
       }
 
-      if (shouldMoveLeft && isWithinXStart) {
-        this.snakePos.push({ x: headX - movementAmount, y: headY });
-        if (!this.eat()) {
-          this.snakePos.shift();
-        }
+      if (shouldMoveLeft) {
+        nextPos.x = headX - movementAmount;
+        moveTo(nextPos);
       }
 
-      if (shouldMoveUp && isWithinYStart) {
-        this.snakePos.push({ x: headX, y: headY - movementAmount });
-        if (!this.eat()) {
-          this.snakePos.shift();
-        }
+      if (shouldMoveUp) {
+        nextPos.y = headY - movementAmount;
+        moveTo(nextPos);
       }
 
-      if (shouldMoveDown && isWithinYEnd) {
-        this.snakePos.push({ x: headX, y: headY + movementAmount });
-        if (!this.eat()) {
-          this.snakePos.shift();
-        }
+      if (shouldMoveDown) {
+        nextPos.y = headY + movementAmount;
+        moveTo(nextPos);
       }
     }
   }
