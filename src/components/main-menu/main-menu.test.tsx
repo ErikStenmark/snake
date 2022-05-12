a6e65bc81754258d9fde242a06360fa2a6e0b82a
erik <erik.stenmark@visma.com>
2022-05-12 23:34:11 +0300

    added more tests

--- src/components/main-menu/main-menu.test.tsx
+++ src/components/main-menu/main-menu.test.tsx
@@ -6,9 +6,11 @@ import MainMenu, { gameTitle } from './main-menu';
 import userEvent from '@testing-library/user-event';
 
 const runSpy = jest.fn();
+const optionsSpy = jest.fn();
 
 const mockEngine = new MockEngine();
 mockEngine.run = runSpy;
+mockEngine.setOptions = optionsSpy;
 
 const wrapper = menuProviderWrapper(mockEngine);
 const { render } = renderFactory(MainMenu, { renderOpts: { wrapper } });
@@ -17,7 +19,14 @@ describe('main-menu', () => {
 
     beforeEach(() => {
         runSpy.mockClear();
+        optionsSpy.mockClear();
     });
+
+    const goToOptions = (container: HTMLElement) => {
+        userEvent.type(container, '{arrowdown}');
+        userEvent.type(container, '{enter}');
+        return container;
+    }
 
     it('should render without crashing', () => {
         render();
@@ -28,6 +37,12 @@ describe('main-menu', () => {
         const { container } = render();
         userEvent.type(container, '{enter}');
         expect(runSpy).toHaveBeenCalled();
+    });
+
+    it('should open options menu', () => {
+        const { container } = render();
+        goToOptions(container);
+        expect(screen.getByText('options')).toBeInTheDocument();
     });
 
 });
